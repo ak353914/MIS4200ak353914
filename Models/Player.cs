425e93a036d021f7398e911ed58404dc507fec7e
Alex Kempen <ak353914@ohio.edu>
2020-02-07 13:05:43 -0500

    Added Player and Team Models (Not Complete)

---
+++ Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200ak353914.Models
+{
+    public class Player
+    {
+        public int playerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string year { get; set; }
+        public string salary { get; set; }
+        public string gamesPlayed { get; set; }
+        public int teamID { get; set; }
+        public virtual Team Team { get; set; }
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200ak353914.Models
+{
+    public class Team
+    {
+        public int teamID { get; set; }
+        public string city { get; set; }
+        public string teamName { get; set; }
+        public string payroll { get; set; }
+        public ICollection<Player> Player { get; set; }
+    }
+}

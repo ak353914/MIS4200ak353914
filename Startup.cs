cd3d35324c3f72314c6f5effb4212ba1b3a99f23
Alex Kempen <ak353914@ohio.edu>
2020-02-07 11:10:49 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS4200ak353914.Startup))]
+namespace MIS4200ak353914
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

bfe4530cc4090f8e653194976c6d1df81391e7a7
Alex Kempen <ak353914@ohio.edu>
2020-02-10 00:17:00 -0500

    Changed

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -12,13 +12,19 @@ namespace MIS4200ak353914.DAL
      
             public MIS4200Context() : base("name=DefaultConnection")
             {
-
+            // add the SetInitializer statement here
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200Context, 
+                MIS4200ak353914.Migrations.MISContext.Configuration>("DefaultConnection"));
             }
+    
             public DbSet<Team> Team { get; set; }
             public DbSet<Player> Player { get; set; }
             public DbSet<Sport> Sport { get; set; }
             public DbSet<PlayerSport> PlayerSport { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
 
-        
     }
 }
